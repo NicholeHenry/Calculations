e11539abcdfadc85ae19922bfdd06e59dba7aa34
Nichole Henry <nichole.henry12@gmail.com>
2019-11-29 12:09:21 -0600

    Add project files.

---
+++ Calculations/Percentage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+   public class Percentage
+    {
+        public static string PercentgeCalc() 
+        {
+
+            Console.WriteLine("Please enter number to be configured");
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter percentage");
+
+            int percent = int.Parse(Console.ReadLine());
+
+            int multipliedNumber = number * percent;
+
+            int result = multipliedNumber / 100;
+
+            string stringResult = result.ToString();
+
+            return stringResult;
+
+        }
+
+
+    }
+}
---
+++ Calculations/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string percentResult = Percentage.PercentgeCalc();
+            Console.WriteLine(percentResult);
+        }
+    }
+}
